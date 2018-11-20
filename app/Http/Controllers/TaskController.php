a987693a0329a27d58dc095bd52843b3d1fe30f6
3A532072 <dinel1138@gmail.com>
2018-11-21 05:35:58 +0800

    刪除任務: 增加刪除紐與路由模型綁定

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -17,22 +17,24 @@ class TaskController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+    	$this->middleware('auth');
     }
+    
     /**
  	 * 顯示使用者所有任務的清單。
  	 *
  	 * @param  Request  $request
  	 * @return Response
  	 */
-	public function index(Request $request)
-	{
-		$tasks = Task::where('user_id', $request->user()->id)->get();
+    public function index(Request $request)
+    {
+    	$tasks = Task::where('user_id', $request->user()->id)->get();
 
-		return view('tasks.index', [
-			'tasks' => $tasks,
-		]);
-	}
+    	return view('tasks.index', [
+    		'tasks' => $tasks,
+    	]);
+    }
+
 	/**
      * 建立新的任務。
  	 *
@@ -50,5 +52,17 @@ class TaskController extends Controller
 		]);
 
 		return redirect('/tasks');
+	}
+
+	/**
+ 	 * 移除給定的任務。
+ 	 *
+ 	 * @param  Request  $request
+ 	 * @param  Task  $task
+ 	 * @return Response
+ 	 */
+	public function destroy(Request $request, Task $task)
+	{
+    //
 	}
 }
